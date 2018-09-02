faffbb93b9c54c31c61ccf99fda72a45ee5f7c09
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2018-09-02 13:20:22 +0530

    Update app.config.ts
    Updated for method 2

--- get-config-on-initialize/src/app/app.config.ts
+++ get-config-on-initialize/src/app/app.config.ts
@@ -1,90 +1,33 @@
-import { Inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { map, catchError } from 'rxjs/operators';
-import { Observable, throwError } from 'rxjs';
+import { environment } from '../environments/environment';
+import { IAppConfig } from './models/app-config.model';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
 
 @Injectable()
 export class AppConfig {
 
-  private config: Object = null;
-  private env: Object = null;
+  static settings: IAppConfig;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  /**
-   * GetConfig
-   * @description Use to get data found in second file (config file)
-   * @param key key to get
-   */
-  public GetConfig(key: any) {
-    return this.config[key];
-  }
-
-  /**
-   * GetEnv
-   * @description Use to get data found in first file (env file)
-   * @param key env to get
-   */
-  public GetEnv(key: any) {
-    return this.env[key];
-  }
-
-  /**
-   * Load
-   * @description This method:
-   *   * Loads `env.json` to get the current working environment
-   * (e.g.: *production*, *development*)
-   *   * Loads `config.[env].json` to get all env's variables
-   * (e.g.: *config.development.json*)
-   */
   public Load() {
-    return new Promise((resolve, reject) => {
-      this.http.get('env.json')
+    const jsonFile = `assets/config/config.${environment.name}.json`;
+    return new Promise<void>((resolve, reject) => {
+      this.http.get<IAppConfig>(jsonFile)
         .pipe(
-          // map( (res: any) => {
-          //   console.log(res);
-          //   return res.json();
-          // }),
-          catchError((error: HttpErrorResponse): any => {
-            console.log('Configuration file "env.json" could not be read');
-            resolve(true);
+          catchError((error: HttpErrorResponse) => {
+            // console.error(`Error reading ${envResponse.env} configuration file`);
+            reject(`Could not load file '${jsonFile}': ${error.message}`);
             return throwError(error.message || 'Server error');
+
           })
         )
-        .subscribe((envResponse: {'env': string}) => {
-          this.env = envResponse;
-          let request: Observable<any> = null;
-
-          switch (envResponse.env) {
-            case 'prod':
-            case 'dev':
-            case 'qa': {
-              request = this.http.get(`config.${envResponse.env}.json`);
-            } break;
-            default: {
-              console.error('Environment file is not set or invalid');
-              resolve(true);
-            } break;
-          }
-
-          if (request) {
-            request
-              .pipe(
-                // .map( res => res.json() ),
-                catchError((error: HttpErrorResponse) => {
-                  console.error(`Error reading ${envResponse.env} configuration file`);
-                  resolve(error);
-                  return throwError(error.message || 'Server error');
-                })
-              )
-              .subscribe((responseData) => {
-                this.config = responseData;
-                resolve(true);
-            });
-          } else {
-            console.error('Env config file "env.json" is not valid');
-            resolve(true);
-          }
+        .subscribe((responseData) => {
+            AppConfig.settings = responseData;
+            resolve();
         });
     });
   }
